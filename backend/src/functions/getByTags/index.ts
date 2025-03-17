91ac89da38153bfcc798a36024a827f323d09a7d
Rolf Sparv <hrothi.wulfaz@gmail.com>
2025-03-17 23:23:13 +0100

    get album by majority consensus tags

--- backend/src/functions/getByTags/index.ts
+++ backend/src/functions/getByTags/index.ts
@@ -1,6 +1,37 @@
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 import db from "../../database/index";
 import { sendResponse, sendError } from "../../responses/index";
 
-exports.handler = async (event: any) => {
-    
+export const handler = async (event: any) => {
+    const { tags } = JSON.parse(event.body);
+
+    try {
+
+        const albumScanCommand = new ScanCommand({
+            TableName: "examProjectAlbums",
+        });
+        const albumScanResult = await db.send(albumScanCommand);
+        const albums = albumScanResult.Items
+
+        const matches = albums?.filter(album => {
+            const globalTags = album.globalTags;
+            const addedBy = [...album.addedBy];
+            if (!addedBy || addedBy.length === 0 || !globalTags) return false;
+            const majorityOfUsers = Math.ceil(addedBy.length/2);
+            return tags.every((tag: string) => (globalTags[tag] || 0) >= majorityOfUsers);
+        });
+
+        const withCorrectSets = matches!.map((item) => {
+            return {
+                ...item,
+                addedBy: [...item.addedBy],
+            }
+        });
+
+        return sendResponse(200, true, "Albums found", withCorrectSets);
+
+    } catch (error) {
+        console.error(error);
+        return sendError(500, "Server error");
+    }
 }
